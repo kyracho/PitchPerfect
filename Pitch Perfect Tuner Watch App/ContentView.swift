21f9da47ccab926cc6d7f2cf4d4e7f73e78c9b68
kyracho <wsc2119@columbia.edu>
2024-08-21 14:53:10 -0700

    Initial Commit

---
+++ Pitch Perfect Tuner Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pitch Perfect Tuner Watch App
+//
+//  Created by Kyra Cho on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pitch Perfect Tuner Watch App/Pitch_Perfect_TunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pitch_Perfect_TunerApp.swift
+//  Pitch Perfect Tuner Watch App
+//
+//  Created by Kyra Cho on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Pitch_Perfect_Tuner_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
